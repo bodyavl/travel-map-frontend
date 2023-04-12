8f3978ea031a04f1256b42746b6fbbb643345069
bodyavl <88792596+bodyavl@users.noreply.github.com>
2023-04-13 00:04:06 +0300

    fetcking markers

--- src/Components/Map/Mapbox.tsx
+++ src/Components/Map/Mapbox.tsx
@@ -1,27 +1,57 @@
 import mapboxgl from 'mapbox-gl';
+import Map, { Marker, Popup } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { useRef, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import s from './Mapbox.module.scss'
 
+interface MapMarker {
+  latitude: number,
+  longitude: number,
+  rating: number,
+  title: string,
+  description: string,
+  _id: string
+}
+const API_URL = 'https://travel-map-api.onrender.com'
+
 const Mapbox = () => {
-    const mapContainer = useRef<any>(null);
-    const map = useRef<mapboxgl.Map | null>(null);
-    const [lng, setLng] = useState(30.523333);
-    const [lat, setLat] = useState(50.45);
-    const [zoom, setZoom] = useState(7);
-    useEffect(() => {
-      mapboxgl.accessToken = import.meta.env.VITE_MAPBOXTOKEN
-        if (map.current) return;
-            map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v12',
-            center: [lng, lat],
-            zoom: zoom
-           });
-        });
+  const [lng, setLng] = useState(30.523333);
+  const [lat, setLat] = useState(50.45);
+  const [zoom, setZoom] = useState(7);
+  const [markers, setMarkers] = useState<Array<MapMarker>>()
+  useEffect(() => {
+    async function fetchMarkers() {
+      let res = await fetch(`${API_URL}/mark`,
+        { headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      }})
+      let result:Array<MapMarker> = await res.json();
+      setMarkers(result);
+    }
+    fetchMarkers();
+  }, []);
+
+
+
+    
+  
   return (
-    <div>
-        <div ref={mapContainer} className={s.map_container} />
+    <div className={s.map_container}>
+      <Map mapboxAccessToken={import.meta.env.VITE_MAPBOXTOKEN}
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+          zoom: zoom
+        }}
+        
+        mapStyle="mapbox://styles/mapbox/streets-v12">
+        {markers?.map((marker, index) => {
+          console.log(marker.title)
+          return (<div key={index}><Marker longitude={marker.longitude} latitude={marker.latitude}/>
+                  <Popup latitude={marker.latitude} longitude={marker.longitude} anchor='left'>You are here</Popup></div>)
+        })}
+      </Map>
     </div>
   )
 }
